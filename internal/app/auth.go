370b43f214d248f8d77a1ad7c346125571ffecfa
Sergey Povarnin <sergey.povarnin@gmail.com>
2026-09-08 23:55:50 +0300

    fix(auth): simplify embedded page selector

--- internal/app/auth.go
+++ internal/app/auth.go
@@ -166,7 +166,7 @@ func (h *AuthHandler) Me(w http.ResponseWriter, r *http.Request) {
 	identity, _ := httpauth.CurrentUserFromRequest(r)
 	data := AuthPageData{Page: view.Page{Title: "Your account", Nav: view.MainNavigation()}, Form: AuthForm{}}
 	view.ApplyRequestState(&data.Page, r)
-	data.Page.CurrentUser = &view.CurrentUser{ID: identity.ID, DisplayName: identity.FirstName}
+	data.CurrentUser = &view.CurrentUser{ID: identity.ID, DisplayName: identity.FirstName}
 	if err := h.renderer.Render(w, http.StatusOK, "me.tmpl", data); err != nil {
 		response.ServerError(w, r, h.logger, fmt.Errorf("render account page: %w", err))
 	}
